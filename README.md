060b64e42d2ecf775d2c01935cd58bfaef0cc8d9
symarys <symarys@gmail.com>
2015-10-16 09:24:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redmine Related Issues plugin (work in progress - do not use!)
+
+#### Plugin which enables adding multiple related issues.
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_related_issues do
+  name 'Redmine Related Issues plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/efigence/redmine_related_issues'
+  author_url 'https://github.com/efigence'
+end
